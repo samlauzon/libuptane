9ebf599522bd03b2c6fbc9b15950d4b82f49af28
Sam Lauzon <slauzon@gmail.com>
2016-09-20 19:40:30 -0400

    Adds ed25519 + sha512 support via the crypto-ed25519 interface wrapper   Made crypto-ed25519 default in build   Added sha512 of webdownload to main for testing (to be removed)

--- libuptane/src/libuptane.c
+++ libuptane/src/libuptane.c
@@ -6,11 +6,26 @@
 #include "libuptane.h"
 #include "interfaces.h"
 
+#include "sha512.h"
+
+
+char ncsha512[1024] = { 0 }; 
+
+
 void webdata_callback( const char *data ) 
 {
+	fprintf(stderr, "Received data from the web; \n");
 	fprintf(stderr, data); 
+	
+	// Verified against http://hash.online-convert.com/sha512-generator
+	int p = sha512(data, strlen(data), &ncsha512); 
+	fprintf(stderr, "\n The sha512 of the data above is: ", ncsha512);  
+	for(int i = 0; i <= 63; i++) {
+      fprintf(stderr, "%X", 0xFF & ncsha512[i]  ); 
+   } 
+	fprintf(stderr, "\n\n"); 
+
 	free( (void *) data); // was malloc'd in the wrapper.
-	fprintf(stderr, "Received data from the web.\n");
 }
 
 void uptane_init( void )
@@ -21,5 +36,6 @@ void uptane_init( void )
 	web_init(webdata_callback); 
 	web_fetch("https://www.samlauzon.com"); 
 	web_fini(); 
+
 }
 
