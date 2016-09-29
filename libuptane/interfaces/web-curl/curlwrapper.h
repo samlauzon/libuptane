10d9c7d360e7ff8667f30dd50b8286f591fdc822
Sam Lauzon <slauzon@umich.edu>
2016-09-28 22:04:19 -0400

    Fixed build (missing stdint.h)    (Requires further investigation)

--- libuptane/interfaces/web-curl/curlwrapper.h
+++ libuptane/interfaces/web-curl/curlwrapper.h
@@ -3,6 +3,8 @@
 #ifndef __curlwrapper_h
 #define __curlwrapper_h
 
+#include <stdint.h> 
+
 void curl_init( void* fn ); 
 size_t fetch_callback(void *contents, size_t size, size_t nmemb, void *userp);
 void curl_setcallback( void *fn );
