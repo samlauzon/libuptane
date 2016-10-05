62bf4279be3fc868e5e8eda2c5f270b69f5ddde6
Sam Lauzon <slauzon@gmail.com>
2016-10-05 18:09:48 -0500

    Fixes client build on gcc 4.0 due to werror flag and missing includes

--- uptane-client/main.c
+++ uptane-client/main.c
@@ -1,9 +1,15 @@
 
 #include "libuptane.h"  // 
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) 
 {
+   fprintf(stderr, "[uptane-client] Calling uptane_init: "); 
    uptane_init( ); // libUptane uptane_init 
+   fprintf(stderr, "\n"); 
+
+   
    
    return 0;
 }
